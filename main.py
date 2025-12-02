1e70a68f15b4a22c7256bc0191e8f9cc27ddf7cf
Matteo-Bormolini <mbormolini@hotmail.com>
2025-12-02 10:33:05 -0300

    Remove comentários de salvamento Git do código

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ from servicosdigitais.app import app
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+''' Salvamento Git
+
+Vamos Salvar no GitHub para não perder acesso:
+
+git add .
+git commit -m "Descricao do que você fez: Exemplo: Implementacao da pagina Clientes"
+git push -u origin main
+
+'''
